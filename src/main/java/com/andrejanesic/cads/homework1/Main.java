831f4f3280769d6d15b230a0d895e804e421fda2
Andreja Nesic <office@andrejanesic.com>
2023-03-29 02:27:23 +0200

    Added Builder to core, update Main

--- src/main/java/com/andrejanesic/cads/homework1/Main.java
+++ src/main/java/com/andrejanesic/cads/homework1/Main.java
@@ -7,33 +7,50 @@ import lombok.Getter;
 
 public class Main {
 
+    private static final Object initializedLock = new Object();
     /**
      * Whether the app has been initialized or not.
      */
     private static boolean initialized = false;
-    private static final Object initializedLock = new Object();
 
     @Getter
     private static Core core;
 
     /**
      * Initializes the program.
+     *
+     * @param args Passed arguments.
      */
-    public static void init() {
+    public static void init(String[] args) {
         if (initialized) return;
         synchronized (initializedLock) {
             if (initialized) return;
 
-            core = new Core(
-                    new ArgsCommonsCLI(),
-                    new CFG4JLoader()
-            );
+            // create core
+            core = Core.builder()
+                    .args(new ArgsCommonsCLI())
+                    .configLoader(new CFG4JLoader())
+                    .build();
+
+            try {
+                // initialize components
+                core.init(args);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
             initialized = true;
         }
     }
 
+    /**
+     * Initializes the program with no passed arguments.
+     */
+    public static void init() {
+        init(new String[]{});
+    }
+
     public static void main(String[] args) {
-        init();
+        init(args);
         System.out.println("Hello world!");
     }
 }
--- src/main/java/com/andrejanesic/cads/homework1/core/Core.java
+++ src/main/java/com/andrejanesic/cads/homework1/core/Core.java
@@ -2,18 +2,32 @@ package com.andrejanesic.cads.homework1.core;
 
 import com.andrejanesic.cads.homework1.args.IArgs;
 import com.andrejanesic.cads.homework1.config.IConfigLoader;
-import lombok.AllArgsConstructor;
+import com.andrejanesic.cads.homework1.core.exceptions.ArgsException;
+import com.andrejanesic.cads.homework1.core.exceptions.ConfigException;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 
 /**
  * Binds all app components together.
  */
-@AllArgsConstructor
+@Builder
+@Getter
 public class Core {
 
-    @Getter
+    @NonNull
     private IArgs args;
 
-    @Getter
+    @NonNull
     private IConfigLoader configLoader;
+
+    /**
+     * Initializes the components.
+     *
+     * @param args Passed arguments.
+     */
+    public void init(String[] args) throws ArgsException, ConfigException {
+        getArgs().parse(args);
+        getConfigLoader().load();
+    }
 }
