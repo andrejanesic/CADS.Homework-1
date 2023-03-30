c6cae376fa30b407ad18f98ce032f0a2a4fba114
Andreja Nesic <office@andrejanesic.com>
2023-03-30 15:08:12 +0200

    Removed old web scanner

--- src/main/java/com/andrejanesic/cads/homework1/config/AppConfiguration.java
+++ src/main/java/com/andrejanesic/cads/homework1/config/AppConfiguration.java
@@ -1,6 +1,7 @@
 package com.andrejanesic.cads.homework1.config;
+
 import com.andrejanesic.cads.homework1.directoryCrawler.IDirectoryCrawler;
-import com.andrejanesic.cads.homework1.webScanner.IWebScanner;
+import com.andrejanesic.cads.homework1.scanner.IWebScanner;
 
 public interface AppConfiguration {
 
@@ -8,30 +9,35 @@ public interface AppConfiguration {
      * List of keywords to search for in the text corpuses. Comma-delimited.
      * Each keyword is counted only if it's standalone (not detected as part of
      * another keyword.)
+     *
      * @return Array of keywords.
      */
     String[] keywords();
 
     /**
      * The prefix that denotes that a directory contains text corpus files.
+     *
      * @return Prefix string.
      */
     String fileCorpusPrefix();
 
     /**
      * Indicates how long the {@link IDirectoryCrawler} should sleep.
+     *
      * @return Sleep time in ms.
      */
     int directoryCrawlerSleepTime();
 
     /**
      * Limits how large the text corpus may be.
+     *
      * @return File size in bytes.
      */
     long fileScanningSizeLimit();
 
     /**
      * Maximum number of hops to be made by the {@link IWebScanner}.
+     *
      * @return Number of hops.
      */
     int hopCount();
@@ -39,6 +45,7 @@ public interface AppConfiguration {
     /**
      * Time after which the list of URLs visited by {@link IWebScanner} is
      * refreshed.
+     *
      * @return Time in ms.
      */
     int urlRefreshTime();
--- src/main/java/com/andrejanesic/cads/homework1/webScanner/IWebScanner.java
+++
@@ -1,8 +0,0 @@
-package com.andrejanesic.cads.homework1.webScanner;
-
-import com.andrejanesic.cads.homework1.core.IComponent;
-
-public abstract class IWebScanner extends IComponent {
-
-
-}
